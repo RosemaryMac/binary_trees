83929b4c3f42f0617b264a6951865fe2c11b9260
RosemaryMac <rmachanja14@gmail.com>
2022-11-15 21:47:32 +0300

    deleting nodes

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /*
- * binary_tree_delete - function to delete traversely
+ * binary_tree_delete - function to delete nodes in a binary tree
  */
 void binary_tree_delete(binary_tree_t *node)
 {
-	if(root == NULL)
-		return 0;
+	if (root == NULL)
+		return (0);
 	binary_tree_delete(node->left);
 	binary_tree_delete(node->right);
 	printf("Deleting Node : %d\n", root->data);
 	free(root);
-	return;
 }
